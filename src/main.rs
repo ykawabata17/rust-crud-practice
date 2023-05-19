cc3b229f8928d835e7f48c14953748ca3f05d4a2
ykawabata17 <ai1063@nara.kosen-ac.jp>
2023-05-20 00:39:31 +0900

    Create Route Handlers

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,9 @@ use std::fmt::Display;
 use std::sync::Mutex;
 
 
+// ###############################################
+// ここからはデータ構造を定義していくフェーズ
+
 // Ticket構造体を定義
 // データをシリアライズ/デシリアライズするためにderiveする
 #[derive(Serialize, Deserialize)]
@@ -60,6 +63,128 @@ impl ResponseError for ErrNold {
 impl Display for ErrNold {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{:?}", self)
+    }
+}
+
+// 
+struct AppState {
+    tickets: Mutex<Vec<Ticket>>,
+}
+
+// ###############################################
+// ここからはルートハンドラーを作っていくフェーズ
+
+// チケットを作成する関数の定義
+#[post("/tickets")]
+async fn post_ticket(req: web::Json<Ticket>, data: web::Data<AppState>) -> impl Responder {
+    let new_ticket = Ticket {
+        id: req.id,
+        author: String::from(&req.author),
+    };
+
+    let mut tickets = data.tickets.lock().unwrap();
+
+    let response = serde_json::to_string(&new_ticket).unwrap();
+
+    tickets.push(new_ticket);
+    HttpResponse::Created()
+        .content_type(ContentType::json())
+        .body(response)
+}
+
+// チケット全て取得する関数の定義
+#[get("/tickets")]
+async fn get_tickets(data: web::Data<AppState>) -> impl Responder {
+    let tickets = data.tickets.lock().unwrap();
+
+    let response = serde_json::to_string(&(*tickets)).unwrap();
+
+    HttpResponse::Ok()
+        .content_type(ContentType::json())
+        .body(response)
+}
+
+// チケットをID指定で取得する関数の定義
+#[get("/tickets/{id}")]
+async fn get_ticket(id: web::Path<u32>, data: web::Data<AppState>) -> Result<Ticket, ErrNold> {
+    let ticket_id: u32 = *id;
+    let tickets = data.tickets.lock().unwrap();
+
+    // チケットIDと一致するチケットをベクターで検索
+    let ticket: Vec<_> = tickets.iter()
+                            .filter(|x| x.id == ticket_id)
+                            .collect();
+    
+    if !ticket.is_empty() {
+        Ok(Ticket {
+            id: ticket[0].id,
+            author: String::from(&ticket[0].author)
+        })
+    } else {
+        let response = ErrNold {
+            id: ticket_id,
+            err: String::from("ticket not found")
+        };
+        Err(response)
+    }
+}
+
+// チケットの情報をIDから書き換える関数の定義
+#[put("/tickets/{id}")]
+async fn update_ticket(id: web::Path<u32>, req: web::Json<Ticket>, data: web::Data<AppState>) -> Result<HttpResponse, ErrNold> {
+    let ticket_id: u32 = *id;
+
+    // 新しいチケットを作成
+    let new_ticket = Ticket {
+        id: req.id,
+        author: String::from(&req.author),
+    };
+
+    let mut tickets = data.tickets.lock().unwrap();
+
+    let id_index = tickets.iter()
+                    .position(|x| x.id == ticket_id);
+
+    match id_index {
+        Some(id) => {
+            let response = serde_json::to_string(&new_ticket).unwrap();
+            tickets[id] = new_ticket;       // ここで更新作業が行われる
+            Ok(HttpResponse::Ok()
+                .content_type(ContentType::json())
+                .body(response)
+            )
+        },
+        None => {
+            let response = ErrNold {
+                id: ticket_id,
+                err: String::from("ticket not found")
+            };
+            Err(response)
+        }
+    }
+}
+
+// チケットIDからそのチケットを削除する関数の定義
+#[delete("/tickets/{id}")]
+async fn delete_ticket(id: web::Path<u32>, data: web::Data<AppState>) -> Result<Ticket, ErrNold> {
+    let ticket_id: u32 = *id;
+    let mut tickets = data.tickets.lock().unwrap();
+
+    let id_index = tickets.iter()
+                    .position(|x| x.id == ticket_id);
+
+    match id_index {
+        Some(id) => {
+            let deleted_ticket = tickets.remove(id);
+            Ok(deleted_ticket)
+        },
+        None => {
+            let response = ErrNold {
+                id: ticket_id,
+                err: String::from("ticket not found")
+            };
+            Err(response)
+        }
     }
 }
 
