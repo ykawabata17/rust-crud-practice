be1b4d0fd4a627e201f45f0d58bebfdc4fe0af21
ykawabata17 <ai1063@nara.kosen-ac.jp>
2023-05-20 00:52:34 +0900

    Comp App

--- src/main.rs
+++ src/main.rs
@@ -188,7 +188,33 @@ async fn delete_ticket(id: web::Path<u32>, data: web::Data<AppState>) -> Result<
     }
 }
 
+// ###############################################
+// ここからはサーバーを作っていくフェーズ
+#[actix_web::main]
+async fn main() -> std::io::Result<()>{
+    let app_state = web::Data::new(AppState{
+        tickets: Mutex::new(vec![
+            Ticket {
+                id: 1,
+                author: String::from("ReLU")
+            },
+            Ticket {
+                id: 2,
+                author: String::from("Bob")
+            },
+        ])
+    });
 
-fn main() {
-    println!("Hello, World!");
+    HttpServer::new(move || {
+        App::new()
+            .app_data(app_state.clone())
+            .service(post_ticket)
+            .service(get_ticket)
+            .service(get_tickets)
+            .service(update_ticket)
+            .service(delete_ticket)
+    })
+    .bind(("127.0.0.1", 8000))?
+    .run()
+    .await
 }
